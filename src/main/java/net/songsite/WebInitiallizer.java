762bbd0e247eaa672aa405c892180e2cc78caeb2
shjune93 <shjune93@gmail.com>
2019-10-16 22:06:40 +0900

    war배포

--- src/main/java/net/songsite/WebInitiallizer.java
+++ src/main/java/net/songsite/WebInitiallizer.java
@@ -1,5 +1,13 @@
 package net.songsite;
 
-public class WebInitiallizer {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class WebInitiallizer extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SongsiteApplication.class);
+	}
 
 }
