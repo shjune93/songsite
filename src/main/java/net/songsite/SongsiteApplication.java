3e6d23e280a135c4c8430519030efac15c4dd463
shjune93 <shjune93@gmail.com>
2020-05-13 08:39:42 +0900

    test2

--- src/main/java/net/songsite/SongsiteApplication.java
+++ src/main/java/net/songsite/SongsiteApplication.java
@@ -2,16 +2,24 @@ package net.songsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
 @EnableJpaAuditing//도메인 클래스 중복제거에 사용되는 코드
-public class SongsiteApplication {
+public class SongsiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SongsiteApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SongsiteApplication.class)
+	}
+	
 	
 	
 
